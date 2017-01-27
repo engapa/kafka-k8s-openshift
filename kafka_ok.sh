6bfa9735b22fd6a1f886e819d433f8d1cfb51f04
Enrique Garcia <engapa@gmail.com>
2017-01-27 15:20:14 +0100

    ZK_clientPort

--- kafka_ok.sh
+++ kafka_ok.sh
@@ -2,7 +2,7 @@
 
 if $KAFKA_ZK_LOCAL;then
   ZK_clientPort=${ZK_clientPort:-2181}
-  OK=$(echo ruok | nc localhost $ZK_CLIENT_PORT)
+  OK=$(echo ruok | nc localhost $ZK_clientPort)
   if [ "$OK" == "imok" ]; then
     eval `bin/kafka-topics.sh --zookeeper localhost:2181 --list`
     exit $?
