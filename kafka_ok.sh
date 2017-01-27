7bf494fc42dcf48eadd7d87a46521734b09d381a
Enrique Garcia <engapa@gmail.com>
2017-01-27 14:10:13 +0100

    ruok and imok for both zookeeper and kafka services

--- kafka_ok.sh
+++ kafka_ok.sh
@@ -1,2 +1,19 @@
 #!/bin/bash -e
 
+if $KAFKA_ZK_LOCAL;then
+  ZK_CLIENT_PORT=${ZK_CLIENT_PORT:-2181}
+  OK=$(echo zk_ruok | nc 127.0.0.1 $ZK_CLIENT_PORT)
+  if [ "$OK" == "zk_imok" ]; then
+    exit 0
+  else
+    exit 1
+  fi
+fi
+
+KAFKA_ADVERTISE_PORT=${KAFKA_ADVERTISE_PORT:-9092}
+OK=$(echo kafka_ruok | nc 127.0.0.1 $KAFKA_PORT)
+if [ "$OK" == "kafka_imok" ]; then
+  exit 0
+else
+  exit 1
+fi
