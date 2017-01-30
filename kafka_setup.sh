bdcae2eb01a84db9836e4a7b749cd000582a1495
Enrique Garcia <engapa@gmail.com>
2017-01-30 15:38:55 +0100

    setup script

--- kafka_setup.sh
+++ kafka_setup.sh
@@ -4,13 +4,15 @@
 
 HOST=`hostname -s`
 DOMAIN=`hostname -d`
-KAFKA_CONF_DIR=${KAFKA_HOME}/config
+KAFKA_HOME=${KAFKA_HOME:-/opt/kafka}
+KAFKA_CONF_DIR=$KAFKA_HOME/config
 DEBUG=${DEBUG_SETUP:-false}
+KAFKA_ZK_LOCAL=${KAFKA_ZK_LOCAL:-true}
+export SERVER_LOG_DIRS=${SERVER_LOG_DIRS:-$KAFKA_HOME/logs}
 
 function config_files() {
 
-  wget -q https://raw.githubusercontent.com/engapa/utils-docker/master/common-functions.sh
-  . common-functions.sh
+  . ${KAFKA_HOME}/bin/kafka_common_functions.sh
 
   # Server
   PREFIX=SERVER_ DEST_FILE=${KAFKA_CONF_DIR}/server.properties DEBUG=${DEBUG} env_vars_in_file
@@ -56,19 +58,6 @@ function zk_local_cluster_setup() {
 
 function check_config() {
 
-  if $KAFKA_ZK_LOCAL;then
-    mkdir -p ${ZK_dataDir} ${ZK_dataLogDir}
-    echo "${SERVER_BROKER_ID}" >> ${ZK_dataDir}/myid
-    if [ ! -z ZK_HEAP_OPTS ]; then
-      sed -r -i "s/(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$ZK_HEAP_OPTS\"/g" ${KAFKA_HOME}/bin/zookeeper-server-start.sh
-      unset ZK_HEAP_OPTS
-    fi
-    echo "unset KAFKA_HEAP_OPTS" >> ${KAFKA_HOME}/bin/zookeeper-server-start.sh
-    # Avoid a node list connection, then we should use local connection
-    export ZK_clientPort=${ZK_clientPort:-2181}
-    export SERVER_ZOOKEEPER_CONNECT="localhost:${ZK_clientPort}"
-  fi
-
   if [ $KAFKA_REPLICAS -gt 1 ];then
     if [[ $HOST =~ (.*)-([0-9]+)$ ]]; then
       NAME=${BASH_REMATCH[1]}
@@ -83,6 +72,21 @@ function check_config() {
     fi
   fi
 
+  if $KAFKA_ZK_LOCAL;then
+    export ZK_dataDir=${ZK_dataDir:-$KAFKA_HOME/zookeeper/data}
+    export ZK_dataLogDir=${ZK_dataLogDir:-$KAFKA_HOME/zookeeper/data-log}
+    mkdir -p ${ZK_dataDir} ${ZK_dataLogDir}
+    echo "${SERVER_BROKER_ID:-0}" >> ${ZK_dataDir}/myid
+    if [ ! -z $ZK_HEAP_OPTS ]; then
+      sed -r -i "s/(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$ZK_HEAP_OPTS\"/g" ${KAFKA_HOME}/bin/zookeeper-server-start.sh
+      unset ZK_HEAP_OPTS
+    fi
+    echo "unset KAFKA_HEAP_OPTS" >> ${KAFKA_HOME}/bin/zookeeper-server-start.sh
+    # Avoid a node list connection, so we should use local connection
+    export ZK_clientPort=${ZK_clientPort:-2181}
+    export SERVER_ZOOKEEPER_CONNECT="localhost:${ZK_clientPort}"
+  fi
+
   if [[ -z "$SERVER_BROKER_ID" ]]; then
     export SERVER_BROKER_ID=-1
   fi
@@ -90,5 +94,3 @@ function check_config() {
 }
 
 check_config && config_files
-
-exec "$@"
