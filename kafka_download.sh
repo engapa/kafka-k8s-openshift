4a3bc66b2253b46e0837bfaae5b0f2c86b32aa38
Enrique Garcia <engapa@gmail.com>
2017-01-30 15:24:16 +0100

    download kafka and script utils

--- kafka_download.sh
+++ kafka_download.sh
@@ -1,9 +1,40 @@
 #!/bin/bash -e
 
-# Download
-mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
-url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
-wget -q "${url}" -O "/tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
-tar xfz /tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz -C /srv
-rm /tmp/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz
-ln -s /srv/kafka_${SCALA_VERSION}-${KAFKA_VERSION} $KAFKA_HOME
+function download_kafka_release () {
+
+  KAFKA_VERSION=${1:-${KAFKA_VERSION}}
+  SCALA_VERSION=${2:-${SCALA_VERSION}}
+  KAFKA_HOME=${3:-${KAFKA_HOME}}
+
+  if [[ -z $KAFKA_VERSION || -z $SCALA_VERSION || -z $KAFKA_HOME ]]; then
+    echo 'KAFKA_VERSION, SCALA_VERSION and KAFKA_HOME are required values.' && exit 1;
+  fi
+
+  wget -q -O /tmp/KEYS https://kafka.apache.org/KEYS
+  gpg -q --import /tmp/KEYS
+  wget -q -O /tmp/kafka.asc \
+    "https://dist.apache.org/repos/dist/release/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz.asc"
+
+  mirror=$(curl --stderr /dev/null https://www.apache.org/dyn/closer.cgi\?as_json\=1 | jq -r '.preferred')
+  url="${mirror}kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz"
+  wget -q -O "/tmp/kafka.tgz" "${url}"
+
+  gpg -q --verify /tmp/kafka.asc /tmp/kafka.tgz
+  tar -xzf /tmp/kafka.tgz --strip-components 1 -C $KAFKA_HOME
+
+  rm -rf /tmp/kafka.{asc,tgz}
+  rm -rf /tmp/KEYS
+  rm -rf $KAFKA_HOME/NOTICE
+  rm -rf $KAFKA_HOME/LICENSE
+  rm -rf $KAFKA_HOME/site-docs
+  rm -rf $KAFKA_HOME/bin/windows
+
+}
+
+function download_kafka_utils() {
+
+  wget -q -O ${KAFKA_HOME}/bin/kafka_common_functions.sh https://raw.githubusercontent.com/engapa/utils-docker/master/common-functions.sh
+
+}
+
+download_kafka_release "$@" && download_kafka_utils
