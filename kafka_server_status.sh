698ef2ce35b59b0a1c33e0a64b2a46b058565e97
Enrique Garcia <engapa@gmail.com>
2017-09-18 00:00:20 +0200

    Deploy/push docker image to dockerhub (latest from master branch)

--- kafka_server_status.sh
+++ kafka_server_status.sh
@@ -6,9 +6,9 @@
 
 timeout -t 30 $KAFKA_HOME/bin/kafka-topics.sh --zookeeper $SERVER_zookeeper_connect --list > /dev/null 2>&1
 if [[ $? -eq 0 ]]; then
- echo -e "\033[32mKafka server is running , :-)\033[0m"
+ echo -e "\033[32mKafka server is running, :-)\033[0m"
  exit 0
 else
- echo -e "\033[31mKafka server is not running , :-(\033[0m"
+ echo -e "\033[31mKafka server is not running, :-(\033[0m"
  exit 1
 fi
