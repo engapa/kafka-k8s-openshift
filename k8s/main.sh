f9c77307281094dccf6e929c3108df2e887392f8
engapa <engapa@gmail.com>
2020-04-26 17:26:25 +0200

    :mega: Update to minikube 1.9.2 and kubernetes 1.18.2

--- k8s/main.sh
+++ k8s/main.sh
@@ -30,7 +30,7 @@ function minikube-install()
   # Download minikube
   curl -L -o minikube https://storage.googleapis.com/minikube/releases/${MINIKUBE_VERSION}/minikube-$DISTRO-amd64
   chmod +x minikube
-  sudo mv minikube /usr/local/bin/
+  sudo install minikube /usr/local/bin/
 
 }
 
@@ -43,7 +43,7 @@ function minikube-run()
   export CHANGE_MINIKUBE_NONE_USER=true
   export KUBECONFIG=$HOME/.kube/config
 
-  sudo -E minikube start --vm-driver=none --cpus 2 --memory 3062 --kubernetes-version=${KUBE_VERSION}
+  sudo -E minikube start --driver=none --cpus 2 --memory 3062 --kubernetes-version=${KUBE_VERSION}
 
   # this for loop waits until kubectl can access the api server that Minikube has created
   for i in {1..150}; do # timeout for 5 minutes
